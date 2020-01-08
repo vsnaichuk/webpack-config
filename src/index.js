2ff97f0eb3901acf167cb3599a66680eaedcf192
Vova <vova77gh@gmail.com>
2020-01-08 21:23:07 +0200

    use 3 module export methods

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log('Hi')
+import AppService from './modules/app.service'
+import {config} from './modules/config'
+import './modules/header.component'
+
+
+
+console.log('Config key: ', config.key)
+
+const service = new AppService('Hello world');
+
+service.log()
