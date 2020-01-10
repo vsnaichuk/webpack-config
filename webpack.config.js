5bebaf48b998ff2d4c6bff02451c52aa69fcd898
Vova <vova77gh@gmail.com>
2020-01-10 02:33:10 +0200

    use html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,17 @@
 const path = require('path')
+var HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
     entry: './src/index.js',
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: './src/index.html'
+        })
+    ]
+
 }
