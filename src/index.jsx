a26aaa8cb7d4a3bc75d97cc77e04d396e52e700e
Vova <vova77gh@gmail.com>
2020-01-25 07:17:38 +0200

    use ESlint and delete JSHint; add new loaders in webpack.config(jsx,ts,js); App and index to .jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,22 +1,27 @@
-import React from 'react'
-import {render} from 'react-dom'
-import AppService from '@modules/app.service'
 import {config} from '@modules/config'
-import '@modules/ts.module'
-import App from './App'
-import users from './users.json'
+import AppService from '@modules/app.service'
 import logo from './icons/logo.png'
+import users from './users.json'
+
 import './css/index.css'
 import './less/index.less'
 import './scss/index.scss'
+
 import $ from 'jquery'
+import React from 'react'
+import {render} from 'react-dom'
+import App from './App.jsx'
+import '@modules/ts.module'
+
 
 console.log('Config key: ', config.key)
 
 const service = new AppService('Hello webpack', logo)
 
+console.log('Show users: ', users)
+
 $('pre').addClass('code').html(service.toString())
 
-console.log('Show users: ', users)
+
 
 render(<App />, document.getElementById('app'))
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const path = require('path')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
@@ -13,29 +13,65 @@ const isProd = !isDev
 
 const optimization = () => {
     const config = {
-      splitChunks: {
-        chunks: 'all'
-      }
+        splitChunks: {
+            chunks: 'all'
+        }
     }
-  
+
     if (isProd) {
-      config.minimizer = [
-        new OptimizeCSSAssetsPlugin(),
-        new TerserJSPlugin()
-      ]
+        config.minimizer = [
+            new OptimizeCSSAssetsPlugin(),
+            new TerserJSPlugin()
+        ]
     }
-  
+
     return config
 }
-  
+
+const cssLoaders = addition => {
+    const loaders = [MiniCssExtractPlugin.loader, 'css-loader']
+
+    if (addition) {
+        loaders.push(addition)
+    }
+
+    return loaders
+}
+
+const jsLoaders = () => {
+    const loaders = [{
+        loader: 'babel-loader',
+        options: babelOptions()
+    }]
+
+    if (isDev) {
+        loaders.push('eslint-loader')
+    }
+
+    return loaders
+}
+
+const babelOptions = preset => {
+    const opts = {
+        presets: [
+            '@babel/preset-env'
+        ]
+    }
+
+    if (preset) {
+        opts.presets.push(preset)
+    }
+
+    return opts
+}
 
 module.exports = {
     context: path.resolve(__dirname, 'src'),
     entry: {
-      main: './index.js',
-      analytics: './analytics.js'
+        main: ['@babel/polyfill', './index.jsx'],
+        analytics: './analytics.js'
     },
-    
+
     output: {
         filename: '[name].[contenthash].js',
         path: path.resolve(__dirname, 'dist')
@@ -53,24 +89,22 @@ module.exports = {
             filename: 'index.html',
             template: './index.html',
             minify: {
-              collapseWhitespace: isProd,
-              removeComments: isProd,
-              removeRedundantAttributes: isProd,
-              removeScriptTypeAttributes: isProd,
-              removeStyleLinkTypeAttributes: isProd,
-              useShortDoctype: isProd
+                collapseWhitespace: isProd,
+                removeComments: isProd,
+                removeRedundantAttributes: isProd,
+                removeScriptTypeAttributes: isProd,
+                removeStyleLinkTypeAttributes: isProd,
+                useShortDoctype: isProd
             }
         }),
         new CleanWebpackPlugin(),
-        new CopyWebpackPlugin([
-          {
+        new CopyWebpackPlugin([{
             from: path.resolve(__dirname, 'src/icons/favicon.ico'),
             to: path.resolve(__dirname, 'dist')
-          }
-        ]),
+        }]),
         new ScriptExtHtmlWebpackPlugin({
-          sync: 'important',
-          defaultAttribute: 'defer'
+            sync: 'important',
+            defaultAttribute: 'defer'
         }),
         new MiniCssExtractPlugin({
             filename: '[name].[contenthash].css',
@@ -78,40 +112,55 @@ module.exports = {
     ],
 
     resolve: {
-      extensions: ['.js','.ts'],
-      alias: {
-        '@modules': path.resolve(__dirname, 'src/modules'),
-        '@': path.resolve(__dirname, 'src')
-      }
+        extensions: ['.js', '.ts'],
+        alias: {
+            '@modules': path.resolve(__dirname, 'src/modules'),
+            '@': path.resolve(__dirname, 'src')
+        }
     },
 
     module: {
-        rules: [
-          {
-            test: /\.css$/,
-            use: [MiniCssExtractPlugin.loader,'css-loader']
-          },
-          {
-            test: /\.less$/,
-            use: [MiniCssExtractPlugin.loader,'css-loader','less-loader']
-          },
-          {
-            test: /\.scss$/,
-            use: [MiniCssExtractPlugin.loader,'css-loader','sass-loader']
-          },
-          { 
-            test: /\.(png|jpg|svg|gif)$/, 
-            use: ['file-loader'] 
-          },
-          { 
-            test: /\.(ttf|woff|woff2|eot)$/, 
-            use: ['file-loader'] 
-          },
-          { 
-            test: /\.(js|ts)$/, 
-            exclude: /node_modules/, 
-            loader: 'babel-loader' 
-          }
+        rules: [{
+                test: /\.css$/,
+                use: cssLoaders()
+            },
+            {
+                test: /\.less$/,
+                use: cssLoaders('less-loader')
+            },
+            {
+                test: /\.s[ac]ss$/,
+                use: cssLoaders('sass-loader')
+            },
+            {
+                test: /\.(png|jpg|svg|gif)$/,
+                use: ['file-loader']
+            },
+            {
+                test: /\.(ttf|woff|woff2|eot)$/,
+                use: ['file-loader']
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: jsLoaders()
+            },
+            {
+                test: /\.ts$/,
+                exclude: /node_modules/,
+                loader: {
+                    loader: 'babel-loader',
+                    options: babelOptions('@babel/preset-typescript')
+                }
+            },
+            {
+                test: /\.jsx$/,
+                exclude: /node_modules/,
+                loader: {
+                    loader: 'babel-loader',
+                    options: babelOptions('@babel/preset-react')
+                }
+            },
         ]
     }
 }
